432202d6e854f2069929cec4088f0b0b444a0288
Victor Pereira Braga <victtor.vpb@gmail.com>
2017-09-16 15:51:59 -0300

    create test get all tasks

--- tests/tests.py
+++ tests/tests.py
@@ -9,3 +9,16 @@ class TestUrlTodoIsAlive(TestCase):
         self.assertEqual(resp.status_code,200)
 
 
+class TestGetTasksSucess(TestCase):
+    def setUp(self):
+        for i in range(1,3):
+            Task.objects.create(description='tasks {}'.format(i))
+
+    def test_index(self):
+        
+        response = self.client.get('http://localhost:8000/todo-list/api/v1/tasks/')
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
